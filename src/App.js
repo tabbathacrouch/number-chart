a1ef51647f8798ab5fb1f00a9712b47d1d632471
Tabbatha Crouch <tabbathacrouch@gmail.com>
2021-08-24 17:01:29 -0500

    minor text changes, added footer

--- src/App.js
+++ src/App.js
@@ -9,20 +9,27 @@ function App() {
         <NumberChart />
       </div>
       <div className="information">
-        Color this interactive number chart to illustrate multiples and
-        patterns. Use white to erase errors. Number charts help teach counting,
-        skip counting, addition, subtraction, and multiplication. They provide a
-        visual guide for early learners to see how numbers are arranged and
-        organized. An interactive number chart similar to the one found:{" "}
-        <a
-          href="https://toytheater.com/hundreds-chart/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          here
-        </a>
-        .
+        <p>
+          Number charts help teach counting, skip counting, addition,
+          subtraction, and multiplication. They provide a visual guide for early
+          learners to see how numbers are arranged and organized. A number chart
+          similar to this one can be found:{" "}
+          <a
+            href="https://toytheater.com/hundreds-chart/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            here
+          </a>
+          .
+        </p>
+        <strong>Instructions:</strong> Color this interactive number chart to
+        illustrate counting or skipping patterns and show multiples. Use white
+        or the 'clear' button to erase errors.
       </div>
+      <footer>
+        Note: this number chart is not optimized for mobile or tablet use.
+      </footer>
     </>
   );
 }
